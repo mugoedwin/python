0c7bc3940c2f893ef44bc1cfd5db4a346f3a2193
mugoedwin <mugoedwin888@gmail.com>
2026-02-12 20:02:45 +0300

    forloop.py

---
+++ forloop.py
@@ -0,0 +1,25 @@
+#for loop-used to iterate through a sequence
+#loop through a list
+fruits=["apple","banana","mango","pineapple"]
+for fruit in fruits:
+     print(fruit)
+#loop through a string
+for x in "hello":
+     print(x)
+#loop through a range
+
+for z in range (5):
+    print(z)
+for a in range (13,17):
+    print(a)
+#break-terminates a loop
+hobbies=["reading","coding","swimming","running"]
+for hobby in hobbies:
+    print(hobby)
+    if hobby=="swimming":
+        break
+
+
+
+
+
