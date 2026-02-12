33f3b72af07558e4d6280646ddb0b9d95c332aea
mugoedwin <mugoedwin888@gmail.com>
2026-02-12 19:58:24 +0300

    datatype.py

---
+++ datatype.py
@@ -0,0 +1,56 @@
+#strings-sequences-of characters
+hobby=("reading")
+message=("hello,good morning")
+print(hobby,message)
+#type()
+print(type(hobby))
+#integers-hold whole numbers=6,78,90,34
+count=56
+print(count)
+print(type(count))
+#float numbers with decimal places 10,7,78.6
+tax=14.67
+print(tax)
+print(type(tax))
+#complex-whole number and imaginary number denoted by j 2+5j
+x=3+7j
+print(x)
+print(type(x))
+#boolean-true or false
+is_student=True
+print(is_student)
+#typeconversion int(),str()
+mynum="10"
+print(mynum)
+print(type(mynum))
+newnum=int(mynum)
+print(type(newnum))
+digit="39"
+print(digit)
+print(type(digit))
+myfloat=float(digit)
+print(type(myfloat))
+#str()
+marks="98"
+print(marks)
+print(type(marks))
+#type conversation
+newmarks=str(marks)
+print(newmarks)
+print(type(newmarks))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
